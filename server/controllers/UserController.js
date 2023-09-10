e4d45c2ab0647d0367e57d47c6bc042443ec4d7c
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-09-10 14:14:39 +0530

    Register API Created

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,6 +1,56 @@
+import { hashPassword } from "../helpers/UserHelper.js"
 import UserModel from "../models/UserModel.js"
 
-export const registerController =()=>{
+export const registerController =async(req,resp)=>{
+   try {
+   const{name,email,password,phone,address} =  req.body
+   if(!name){
+    resp.send({error:"Name is required"})
+   }
+   if(!email){
+    resp.send({error:"Email is required"})
+   }
+   if(!password){
+    resp.send({error:"Password is required"})
+   }
+   if(!phone){
+    resp.send({error:"Phone is required"})
+   }
+   if(!address){
+    resp.send({error:"Address is required"})
+   }
+   
+//   Check user
 
+   const existingUser = await UserModel.findOne({email})
+
+//  Existing User
+
+   const hashedPassword = await hashPassword(password)
+
+//    Save
+
+   const user = await new UserModel({name,email,phone,address,password:hashedPassword}).save()
+
+   resp.status(201).send({
+    success:true,
+    message:"User Registered Successfully",
+    user,
+   })
+
+   if(existingUser){
+    return resp.status(200).send({
+        success:true,
+        message:'Already Registered please login',
+    })
+   }
+   } catch (error) {
+    console.log(error)
+    resp.status(500).send({
+        success:false,
+        message: "Error in Registration",
+        error
+    })
+   }
 }
 
