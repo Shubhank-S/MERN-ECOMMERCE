375c729040187f2a097a4f3033900a5aeeb8a030
Shubhank Sharma <shubhanksharma345@gmail.com>
2023-09-12 12:57:22 +0530

    Login API created with JWT

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,5 +1,6 @@
-import { hashPassword } from "../helpers/UserHelper.js"
+import { comparePassword, hashPassword } from "../helpers/UserHelper.js"
 import UserModel from "../models/UserModel.js"
+import JWT from 'jsonwebtoken';
 
 export const registerController =async(req,resp)=>{
    try {
@@ -52,5 +53,69 @@ export const registerController =async(req,resp)=>{
         error
     })
    }
+}
+
+// LOGIN || POST
+
+export const loginController =async(req,resp)=>{
+  try {
+    const {email,password} = req.body
+
+   //  Validation
+
+   if(!email,!password){
+      return resp.status(404).send({
+         success:false,
+         message: 'Invalid Email or Password',
+
+      })
+   }
+   // Decrupt the Password
+
+   // Check user
+
+   const user = await UserModel.findOne({email})
+
+   if(!user){
+      return resp.status(404).send({
+         success:false,
+         message:'Email is not Registerd'
+      })
+   }
+
+   const match = await comparePassword(password,user.password)
+     if(!match){
+      return resp.status(200).send({
+         success:false,
+         message: 'Invalid Password'
+      })
+     }
+     
+   //   JWT Token
+
+   const token = await JWT.sign({_id:user._id}, process.env.JWT_SECRET , {
+      expiresIn: "7d"
+   });
+
+resp.status(200).send({
+   success:true,
+   message: 'Login SuccessFully',
+   user:{
+     name: user.name,
+     email: user.email,
+     phone: user.phone,
+     address: user.address,
+   },
+   token,
+})
+
+  } catch (error) {
+   console.log(error)
+   resp.status(500).send({
+      success:false,
+      message: 'Error in Login',
+      error,
+   })
+  }
 }
 
